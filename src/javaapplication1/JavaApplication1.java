68ca4db22a9e3dd9146ad97df6a657478a83a875
fjzom <f.oviedo.m@hotmail.com>
2014-12-12 10:16:11 -0600

    new commit6

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -17,7 +17,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("ho la bv ");
+        System.out.println("ho la bgv ");
         
     }
     
